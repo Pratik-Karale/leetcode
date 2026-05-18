21f99be4a7c930ad7dc5ddf0d093586a3a13050a
Pratik Manoj Karale <prakar57@gmail.com>
2026-05-18 19:09:55 +0530

    Time: 3 ms (88.99%) | Memory: 22.8 MB (62.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,30 @@
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         myMap={}
+#         # for i in range(k):
+#         #     myMap[i]=
+#         for i in range(len(nums)):
+#             if(nums[i] in myMap):
+#                 myMap[nums[i]]+=1
+#             else:
+#                 myMap[nums[i]]=1
+        
+#         arr1=sorted(map(lambda x:(x[1],x[0]),myMap.items()))
+#         return list(map(lambda t:t[1],arr1[-k:]))
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        myMap={}
-        # for i in range(k):
-        #     myMap[i]=
-        for i in range(len(nums)):
-            if(nums[i] in myMap):
-                myMap[nums[i]]+=1
-            else:
-                myMap[nums[i]]=1
-        
-        arr1=sorted(map(lambda x:(x[1],x[0]),myMap.items()))
-        return list(map(lambda t:t[1],arr1[-k:]))
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
