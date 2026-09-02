b6a135a68b315ee2b8a30a92fadd1c5242a391e5
Pratik Manoj Karale <prakar57@gmail.com>
2026-09-02 23:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (24.94%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,33 +5,52 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        prev=None
-        curr=head
-        while curr:
-            tmp=curr.next
-            curr.next=prev
-            prev=curr
-            curr=tmp
-        curr=prev
-        head=prev
-        # print(curr)
-        prev=None
-        c=1
-        while curr:
-            if(c==n):
-                if(prev==None):
-                    head=head.next
-                    break
-                prev.next=curr.next
-                break
-            prev=curr
-            curr=curr.next
-            c+=1
-        prev=None
-        curr=head
-        while curr:
-            tmp=curr.next
-            curr.next=prev
-            prev=curr
-            curr=tmp
-        return prev
+        # ACCEPTED BUT REQUIRED 3 full passes
+        # prev=None
+        # curr=head
+        # while curr:
+        #     tmp=curr.next
+        #     curr.next=prev
+        #     prev=curr
+        #     curr=tmp
+        # curr=prev
+        # head=prev
+        # # print(curr)
+        # prev=None
+        # c=1
+        # while curr:
+        #     if(c==n):
+        #         if(prev==None):
+        #             head=head.next
+        #             break
+        #         prev.next=curr.next
+        #         break
+        #     prev=curr
+        #     curr=curr.next
+        #     c+=1
+        # prev=None
+        # curr=head
+        # while curr:
+        #     tmp=curr.next
+        #     curr.next=prev
+        #     prev=curr
+        #     curr=tmp
+        # return prev
+
+        # 2 pointer approach
+        dummy=ListNode(0,head)
+        p1=dummy
+        p2=head
+
+        for _ in range(n):
+            # if(p2):
+            #     print(p2.val)
+            # else:
+            #     print("Nowdwedne")
+            p2=p2.next
+            
+        while p2:
+            p1=p1.next
+            p2=p2.next
+        p1.next=p1.next.next
+        return dummy.next
